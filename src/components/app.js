3366345bb619d5349b9138cef11a2268de2180fb
lawyerh <lawyerh_01@yahoo.com>
2018-05-09 17:24:29 -0700

    Working version

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; //Added
+import { createStore } from 'redux'; //Added
+import rootReducer from '../reducers/index'; //Added
 
-
-
+const store = createStore(rootReducer) //Added
 
 import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
     return (
-
+      <Provider store={store}> 
         <div>
           <BookList />
         </div>
-
+      </Provider>
     );
   }
 };
